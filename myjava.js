9eaaed38af2fcd9dd588cdfd6fe208e7be0c2c92
SDFcl <142718493+SDFcl@users.noreply.github.com>
2023-08-24 20:27:46 +0700

    Update วิชา6

--- myjava.js
+++ myjava.js
@@ -9,10 +9,13 @@ function summit() {
     var gid4 = document.getElementById("gid4").value;
     var grade5 = document.getElementById("grade5").value;
     var gid5 = document.getElementById("gid5").value;
+    var grade6 = document.getElementById("grade6").value;
+    var gid6 = document.getElementById("gid6").value;
+    
     var sgrade,ss,sgid;
     
-    sgrade = (grade1*gid1)+(grade2*gid2)+(grade3*gid3)+(grade4*gid4)+(grade5*gid5);
-    sgid = (gid1*1)+(gid2*1)+(gid3*1)+(gid4*1)+(gid5*1);
+    sgrade = (grade1*gid1)+(grade2*gid2)+(grade3*gid3)+(grade4*gid4)+(grade5*gid5)+(grade6*gid6);
+    sgid = (gid1*1)+(gid2*1)+(gid3*1)+(gid4*1)+(gid5*1)+(gid6*1);
     ss = sgrade/sgid
     document.getElementById("demo").innerHTML = ss.toFixed(2);
     
