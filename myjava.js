107fd00afa884b9f80ee5e15d8732fecb1c77eda
SDFcl <142718493+SDFcl@users.noreply.github.com>
2024-02-04 12:21:03 +0700

    Update myjava.js change quantity

--- myjava.js
+++ myjava.js
@@ -1,22 +1,44 @@
+function ct() {
+    var num = document.getElementById("numtr").value;
+    var text = "";
+    var table = "<center><table>";
+    var table2 = "</table></center>";
+
+    for (let i = 0; i < num; i++) {
+        text += "<tr style='border-bottom : 2px solid #FF9933;'><th>Sub " + (i+1) + ".</th><th style='width: 90px;'><input name='num' size='1' type='number' id='grade" + (i+1) + "' style='width: 50px;' placeholder='Grade'></th><th style='width: 90px;'><input type='number' id='gid" + (i+1) + "' style='width: 50px;' step='0.1' placeholder='Credit" + (i+1) + "'></th></tr>";
+    }
+    table += text;
+    table += table2;
+    document.getElementById("ctr").innerHTML = table;
+    document.getElementById("butct").innerHTML = "<button onclick='summit()'>Start calculating</button><button onclick='re()'>Re</button>";
+    document.getElementById("pls").innerHTML = "Please enter your grade";
+}
+
+function re() {
+    document.getElementById("ctr").innerHTML = "<input  size='1' type='number' id='numtr' style='width: 110px;' placeholder='How mara Supject?''>";
+    document.getElementById("butct").innerHTML = "<button onclick='ct()'>Create a table</button>";
+    document.getElementById("pls").innerHTML = "How mara Supject?";
+    
+}
+
 function summit() {
-    var grade1 = document.getElementById("grade1").value;
-    var gid1 = document.getElementById("gid1").value;
-    var grade2 = document.getElementById("grade2").value;
-    var gid2 = document.getElementById("gid2").value;
-    var grade3 = document.getElementById("grade3").value;
-    var gid3 = document.getElementById("gid3").value;
-    var grade4 = document.getElementById("grade4").value;
-    var gid4 = document.getElementById("gid4").value;
-    var grade5 = document.getElementById("grade5").value;
-    var gid5 = document.getElementById("gid5").value;
-    var grade6 = document.getElementById("grade6").value;
-    var gid6 = document.getElementById("gid6").value;
-    
-    var sgrade,ss,sgid;
-    
-    sgrade = (grade1*gid1)+(grade2*gid2)+(grade3*gid3)+(grade4*gid4)+(grade5*gid5)+(grade6*gid6);
-    sgid = (gid1*1)+(gid2*1)+(gid3*1)+(gid4*1)+(gid5*1)+(gid6*1);
-    ss = sgrade/sgid
-    document.getElementById("demo").innerHTML = ss.toFixed(2);
-    
+    var numG = document.getElementsByName("num").length;
+    const grade = [];
+    const gid = [];
+    var sgrade = 0;
+    var sgid = 0;
+    var ss = 0;
+
+
+    for (let i = 0; i < numG; i++ ) {
+        var ingrade = document.getElementById("grade" + (i+1)).value;
+        grade.push(ingrade);
+        var ingid = document.getElementById("gid" + (i+1)).value;
+        gid.push(ingid);
+
+        sgrade = sgrade + (grade[i]*gid[i]);
+        sgid = sgid + (gid[i]*1);
+        ss = sgrade/sgid;
+    }
+    document.getElementById("demo").innerHTML = ss.toFixed(2); 
 }
